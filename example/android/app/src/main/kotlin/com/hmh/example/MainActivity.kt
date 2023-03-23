17a3e0e18c2d05d210c24c120b54473f7e288fce
Hasan M. Hallak <hasanm.hallak@gmail.com>
2023-03-24 00:19:21 +0200

    add example

---
+++ example/android/app/src/main/kotlin/com/hmh/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hmh.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:dialogs/dialogs.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Builder(
+        builder: (context) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      context.showModalDialog(
+                        builder: (context) => AlertDialog(
+                          title: const Text('Alert!'),
+                          content: const Text('Something happened!'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                context.dismiss();
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: const Text('Show Alert'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.showToast('Hello World!');
+                    },
+                    child: const Text('Show Toast'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
